2efe9c37e85b24c0983ad7a7aeaab9b6872e2230
Yauheni Buhaev <buhaev.yauheni@gmail.com>
2022-08-31 14:19:51 +0300

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ce7826bd-060f-41a4-aec6-68a1b716c061';
+export const MODAL_CLOSE_SEC = 2.5;
